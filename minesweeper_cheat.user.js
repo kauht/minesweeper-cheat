1ef89899e08fb24f65b4d294cc7e45fcdddfda20
kauht <macinnes.samuel1@gmail.com>
2025-08-07 20:05:31 -0500

    Init

---
+++ minesweeper_cheat.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Automatically highlights safe cells (green) and mines (red) in Minesweeper Online
+// @author       You
+// @match        https://minesweeper.online/*
+// @grant        none
+// ==/UserScript==
+
+// ==UserScript==
+// @name        minesweeper.online cheat
+// @namespace   http://tampermonkey.net/
+// @version     08/07/2025
+// @description Click 'A' for auto-play
+// @author      kauht
+// @grant       none
+// @downloadURL 
+// @updateURL   
+// ==/UserScript==
